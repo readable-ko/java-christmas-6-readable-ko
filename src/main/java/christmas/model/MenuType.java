598d735390b820342fb188a7c4ca9b04eecf6422
readable-ko <21700026@handong.edu>
2023-11-14 01:13:32 +0900

    feat(model): create MenuType.java enum
    구현된 목록
    - 각 메뉴에 맞는 금액과 문자열
    - 문자열로 enum값을 찾아 반환하는 메소드
    - None인지 확인 후 반환하는 메소드
    - findMenuByTitle 반환 결과 test
    - isNone 반환 결과 test

---
+++ src/main/java/christmas/model/MenuType.java
@@ -0,0 +1,47 @@
+package christmas.model;
+
+import java.util.Arrays;
+
+public enum MenuType {
+    NONE("없음", 0),
+    MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000),
+    T_BONE_STEAK("티본스테이크", 55000),
+    BARBEQUE_RIP("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CHOCOLATE_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+    ZERO_COKE("제로콜라", 3000),
+    RED_WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000);
+
+    private final String title;
+    private final long price;
+
+    MenuType(String title, long price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public static MenuType findMenuByTitle(String menu) {
+        return Arrays.stream(values())
+                .filter(menuType -> menuType.title.equals(menu))
+                .findFirst()
+                .orElse(NONE);
+
+    }
+
+    public boolean isNone() {
+        return title.equals(NONE.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getPrice(int number) {
+        return price * number;
+    }
+}
---
+++ src/test/java/christmas/model/MenuTypeTest.java
@@ -0,0 +1,44 @@
+package christmas.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("LottoRank 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class MenuTypeTest {
+    @ParameterizedTest
+    @MethodSource("provideStringWithMatchingMenuType")
+    void 문자열이_포함되어_있으면_해당_MenuType을_반환(String title, MenuType menuType) {
+        assertThat(MenuType.findMenuByTitle(title)).isEqualTo(menuType);
+    }
+
+    private static Stream<Arguments> provideStringWithMatchingMenuType() {
+        return Stream.of(
+                Arguments.of("티본스테이크", MenuType.T_BONE_STEAK),
+                Arguments.of("양송이수프", MenuType.MUSHROOM_SOUP),
+                Arguments.of("시저샐러드", MenuType.CAESAR_SALAD),
+                Arguments.of("아이스크림", MenuType.ICE_CREAM)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"백본스테이크", "소송이수프", "시저시저샐러드", "어른스크림"})
+    void 문자열이_포함되지_않으면_None을_반환(String title) {
+        assertThat(MenuType.findMenuByTitle(title)).isEqualTo(MenuType.NONE);
+    }
+
+    @Test
+    void None이면_true를_반환_그_외에는_false_반환() {
+        assertThat(MenuType.NONE.isNone()).isTrue();
+        assertThat(MenuType.BARBEQUE_RIP.isNone()).isFalse();
+    }
+}
