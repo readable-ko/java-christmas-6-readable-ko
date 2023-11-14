f2b92af3113d9c91d7fea998f5fa2c7b9e3dc839
readable-ko <21700026@handong.edu>
2023-11-14 18:24:30 +0900

    feat(utils): create Message class
    구현된 내용
    - Message 클래스 구현 및 상수 별도 처리

---
+++ src/main/java/christmas/utils/Message.java
@@ -0,0 +1,18 @@
+package christmas.utils;
+
+public class Message {
+    private static final String ENTER = "\n";
+    public static final String ONE_GIVE_AWAY = "1개";
+    public static final String ASK_DATE_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
+            + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String ASK_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String AFTER_DATE_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String AFTER_MENU_MESSAGE = ENTER + "<주문 메뉴>";
+    public static final String ORDER_AMOUNT_MESSAGE = ENTER + "<할인 전 총주문 금액>";
+    public static final String GIVE_AWAY_MESSAGE = ENTER + "<증정 메뉴>";
+    public static final String TOTAL_CHARGE_AMOUNT = ENTER + "<할인 후 예상 결제 금액>";
+    public static final String TOTAL_DISCOUNT = ENTER + "<총혜택 금액>";
+    public static final String DISCOUNT_HISTORY = ENTER + "<혜택 내역>";
+    public static final String DISCOUNT_MESSAGE = "%s";
+    public static final String EVENT_BADGE = ENTER + "<12월 이벤트 배지>";
+}
