026c7893cdebdd12559c3c89651fc310ca0768d0
readable-ko <21700026@handong.edu>
2023-11-14 18:24:42 +0900

    feat(utils): create MagicNumber class
    구현된 내용
    - MagicNumber 클래스 구현 및 상수 별도 처리

---
+++ src/main/java/christmas/utils/MagicNumber.java
@@ -0,0 +1,15 @@
+package christmas.utils;
+
+public class MagicNumber {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int MAX_MENU_VALUE = 20;
+    public static final int LAST_CHRISTMAS_SALE_DAY = 25;
+    public static final int MAX_CALENDAR_DAY = 31;
+    public static final int DAY_SALE_AMOUNT = 100;
+    public static final int THOUSAND = 1000;
+    public static final int DISCOUNT_AMOUNT = 2023;
+    public static final int MIN_DISCOUNT_AMOUNT = 10000;
+    public static final int MIN_BENEFIT_AMOUNT = 120000;
+}
