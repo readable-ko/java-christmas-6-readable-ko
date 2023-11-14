720c0752268e1e8b32ced6bb05d22b0d973b1607
readable-ko <21700026@handong.edu>
2023-11-14 18:02:29 +0900

    feat(model): create Date class
    구현된 내용
    - DateEventType 관리
    - 빨간날, 크리스마스전날 여부 확인 메소드

---
+++ src/main/java/christmas/model/Date.java
@@ -0,0 +1,39 @@
+package christmas.model;
+
+import static christmas.utils.MagicNumber.LAST_CHRISTMAS_SALE_DAY;
+import static christmas.utils.ErrorMessage.IS_NOT_INTEGER_DATE;
+
+import christmas.utils.IntegerParser;
+import christmas.utils.Validate;
+
+public class Date {
+    private final DateEventType dateEventType;
+    private final int day;
+
+    public Date(String date) {
+        day = IntegerParser.convert(date, IS_NOT_INTEGER_DATE);
+        validation();
+
+        dateEventType = DateEventType.findDateOfWeek(day);
+    }
+
+    private void validation() {
+        Validate.isInDateRange(day);
+    }
+
+    public boolean isBeforeDDay() {
+        return day <= LAST_CHRISTMAS_SALE_DAY;
+    }
+
+    public boolean isHoliday() {
+        return day == LAST_CHRISTMAS_SALE_DAY || dateEventType == DateEventType.SUNDAY;
+    }
+
+    public DateEventType getDateType() {
+        return dateEventType;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
---
+++ src/test/java/christmas/model/DateTest.java
@@ -0,0 +1,35 @@
+package christmas.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("DateEventType")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class DateTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"3.14", "hello", "45", "-1"})
+    void 생성자_예외값_입력시_예외_처리(String argument) {
+        assertThatThrownBy(() -> new Date(argument)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"25", "3"})
+    void 특별할인_날짜_확인_테스트(String argument) {
+        assertThat(new Date(argument).isHoliday()).isTrue();
+        assertThat(new Date(argument).isBeforeDDay()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"26", "27", "28", "29", "30"})
+    void 특별할인_날짜_아닌_경우_테스트(String argument) {
+        assertThat(new Date(argument).isHoliday()).isFalse();
+        assertThat(new Date(argument).isBeforeDDay()).isFalse();
+    }
+}
